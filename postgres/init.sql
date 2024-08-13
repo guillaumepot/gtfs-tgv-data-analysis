71b86ebe98fa06e8277c52feca10b6e5890852a8
guillaumepot <guillaume.pot@outlook.com>
2024-08-13 09:02:13 +0200

    compose minor update

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,17 +2,8 @@
 DO
 $$
 BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db1') THEN
-      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE db1');
-   END IF;
-END
-$$;
-
-DO
-$$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'db2') THEN
-      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE db2');
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'train_delay_db') THEN
+      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE train_delay_db');
    END IF;
 END
 $$;
