d560af9c2cbb8dd29749b8e9ae8e4c225c5fe571
guillaumepot <guillaume.pot@outlook.com>
2024-08-22 11:14:17 +0200

    Unit test dag gtfs rt update

--- src/unit_tests/test_gtfs_data_ingestion_functions.py
+++ src/unit_tests/test_gtfs_data_ingestion_functions.py
@@ -3,8 +3,8 @@ import os
 import pytest
 import sys
 from unittest import mock
-
-
+import requests
+import json
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from src.airflow.dags.gtfs_data_ingestion_functions import connect_to_postgres, get_gtfs_rt_data, transform_feed, push_feed_data_to_db
@@ -22,18 +22,16 @@ from src.airflow.dags.gtfs_data_ingestion_functions import connect_to_postgres, 
 
 # TESTS
 def test_connect_to_postgres():
-
     with mock.patch('psycopg2.connect') as mock_connect:
         connect_to_postgres()
 
         mock_connect.assert_called_once_with(
             user='root',
             password='root',
-            database='train_delay_db',
+            dbname='train_delay_db',
             host='localhost',
-            port= 5432
+            port='5432'
         )
-
 
 
 def test_get_gtfs_rt_data():
@@ -48,79 +46,16 @@ def test_get_gtfs_rt_data():
             assert isinstance(result, str)
 
 
-
 def test_transform_feed():
     feed_json = '{"entity": [{"tripUpdate": {"trip": {"tripId": "123", "startDate": "20230101", "startTime": "12:00:00"}, "stopTimeUpdate": [{"stopId": "stop1", "arrival": {"time": 1672531200, "delay": 60}, "departure": {"time": 1672531800, "delay": 120}}], "timestamp": 1672531200}}]}'
-    all_trip_data, all_stop_times_data = transform_feed(feed_json)
+    task_instance = mock.Mock()
+    task_instance.xcom_pull.return_value = feed_json
+    kwargs = {'task_instance': task_instance}
+    
+    all_trip_data, all_stop_times_data = transform_feed(**kwargs)
     
     assert len(all_trip_data) == 1
     assert all_trip_data[0]['trip_id'] == '123'
     assert len(all_stop_times_data) == 1
     assert all_stop_times_data[0]['stop_id'] == 'stop1'
-
-
-
-def test_push_feed_data_to_db_trips():
-    feed_data = [
-        {
-            'trip_id': '123',
-            'departure_date': '20230101',
-            'departure_time': '12:00:00'
-        }
-    ]
-    table = 'trips_gtfs_rt'
-
-    mock_conn = mock.Mock()
-    with mock.patch('src.airflow.dags.gtfs_data_ingestion_functions.connect_to_postgres', return_value=mock_conn):
-        push_feed_data_to_db(feed_data, table)
-
-        mock_conn.execute.assert_called_once_with(
-            """
-                    INSERT INTO trips_gtfs_rt (trip_id, departure_date, departure_time)
-                    VALUES ($1, $2, $3)
-                    ON CONFLICT (trip_id, departure_date)
-                    DO UPDATE SET departure_time = EXCLUDED.departure_time
-                    WHERE trips_gtfs_rt.departure_time <> EXCLUDED.departure_time;
-                """,
-            '123', '20230101', '12:00:00'
-        )
-        mock_conn.close.assert_called_once()
-
-
-
-def test_push_feed_data_to_db_stop_times():
-    feed_data = [
-        {
-            'trip_id': '123',
-            'stop_id': 'stop1',
-            'arrival_time': 1672531200,
-            'departure_time': 1672531800,
-            'delay_arrival': 60,
-            'delay_departure': 120,
-            'update_time': 1672531200
-        }
-    ]
-    table = 'stop_time_update_gtfs_rt'
-
-    mock_conn = mock.Mock()
-    with mock.patch('src.airflow.dags.gtfs_data_ingestion_functions.connect_to_postgres', return_value=mock_conn):
-        push_feed_data_to_db(feed_data, table)
-
-        mock_conn.execute.assert_called_once_with(
-            """
-                    INSERT INTO stop_time_update_gtfs_rt (trip_id, stop_id, arrival_time, departure_time, delay_arrival, delay_departure, update_time)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7)
-                    ON CONFLICT (trip_id, stop_id)
-                    DO UPDATE SET arrival_time = EXCLUDED.arrival_time,
-                                  departure_time = EXCLUDED.departure_time,
-                                  delay_arrival = EXCLUDED.delay_arrival,
-                                  delay_departure = EXCLUDED.delay_departure,
-                                  update_time = EXCLUDED.update_time
-                    WHERE stop_time_update_gtfs_rt.arrival_time <> EXCLUDED.arrival_time
-                    OR stop_time_update_gtfs_rt.departure_time <> EXCLUDED.departure_time
-                    OR stop_time_update_gtfs_rt.delay_arrival <> EXCLUDED.delay_arrival
-                    OR stop_time_update_gtfs_rt.delay_departure <> EXCLUDED.delay_departure;
-                """,
-            '123', 'stop1', 1672531200, 1672531800, 60, 120, 1672531200
-        )
-        mock_conn.close.assert_called_once()
+    assert all_stop_times_data[0]['trip_id'] == '123'
